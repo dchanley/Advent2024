8aa8548261cf0def0aed47a5bf9ed9a1f9338908
dchanley <111655844+dchanley@users.noreply.github.com>
2024-12-03 08:48:53 -0700

    Day 3 done!

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution1 solution1 = new Solution1();
+        }
+    }
+}
---
+++ Day3/Day3/Solution1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Solution1
+    {
+        public Solution1() {
+            solution();
+        }
+        public void solution()
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+            Regex pattern = new Regex("mul\\(\\d+,\\d+\\)");
+            List<int> results = new List<int>();
+            foreach (string line in lines)
+            {                
+                MatchCollection matches = pattern.Matches(line);
+                foreach (Match match in matches)
+                {
+                    string[] temp =match.ToString().Trim().Replace("mul", "").Replace("(", "").Replace(")", "").Split(',');
+                    results.Add(Convert.ToInt32(temp[0])* Convert.ToInt32(temp[1]));                    
+                }                
+            }
+            int solution = 0;
+            foreach(int i in results)
+            {
+                solution += i;
+            }
+            Console.WriteLine(solution);
+            Console.Read();
+        }
+    }
+}
